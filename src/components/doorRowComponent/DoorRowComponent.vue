fbd242c6cfc686c717f64bd5001e224b9f38b6b6
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-10-27 21:24:55 +0100

    added doorRowComponent

---
+++ src/components/doorRowComponent/DoorRowComponent.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="row">
+  <div class="col" v-for="item in this.doorItems" :key="item.day">
+    <DoorComponent v-if="!item.isDummy" :door-item="item"/>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import DoorItem from "../doorComponent/DoorItem"
+import DoorComponent from "@/components/doorComponent/DoorComponent.vue"
+
+export default defineComponent({
+      name: "DoorRow",
+      components: {DoorComponent},
+      props: {
+        doorItems: Object as () => DoorItem[]
+      }
+})
+
+</script>
+
+<style scoped>
+
+</style>
