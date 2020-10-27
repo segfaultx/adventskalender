1e5c2c3852a95e7f2f448f371bfbd12a52babdc4
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-10-27 22:41:59 +0100

    added testdata, used in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <DoorContainer/>
+  <DoorContainer :calendar-items="basicCalendarItems"/>
 </template>
 
 <script lang="ts">
 import "bootstrap/dist/css/bootstrap.min.css"
-import { defineComponent } from "vue"
-import DoorContainer from "@/components/DoorContainer.vue"
+import {defineComponent} from "vue"
+import DoorContainer from "@/components/doorContainer/DoorContainer.vue"
+import CalendarItems from "@/components/doorContainer/CalendarItems"
+import calendarContent from "@/data/calendarContent.json"
 
 export default defineComponent({
   name: "App",
   components: {
     DoorContainer
+  },
+  props: {
+    basicCalendarItems: {
+      type: Object as () => CalendarItems,
+      default: calendarContent
+
+    }
   }
 })
 </script>
@@ -20,6 +29,7 @@ body {
   background: url("~@/assets/background.jpg") no-repeat center center fixed;
   background-size: cover;
 }
+
 @font-face {
   font-family: 'ChristmasFont';
   src: local("ChristmasFont"), url('~@/assets/Reindeer Christmas - Personal Use.otf') format("opentype");
