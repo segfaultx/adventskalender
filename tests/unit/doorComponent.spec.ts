50df5f7fc5a28ae6e484b225728a5977b6a0a567
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-11-02 17:03:53 +0100

    update doorComponent test

--- tests/unit/doorComponent.spec.ts
+++ tests/unit/doorComponent.spec.ts
@@ -3,15 +3,17 @@ import DoorComponent from "@/components/door/DoorComponent.vue"
 import DoorItem from "@/components/door/DoorItem"
 jest.mock("@/components/door/doorFunctions.ts")
 
-const props: DoorItem = {day: 25, content: "hi"}
+const props: DoorItem = {day: 25, content: "hi", backgroundImage: ""}
 
 //TODO: properly mock doorFunctions module to be able to do more testing
 
 describe("DoorComponent.vue", () => {
     it("renders a vue instance", () => {
+        // @ts-ignore
         expect(shallowMount(DoorComponent, {props:props})).not.toBeNull()
     })
     it("renders number correctly", () => {
+        // @ts-ignore
         const wrapper = shallowMount(DoorComponent, {props: props})
         expect(wrapper.find("#door-front").text()).toBe(props.day.toString())
     })
